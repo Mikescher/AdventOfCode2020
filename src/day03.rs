7a5ea19210090d129cc3aa2c8f95f6cf4f0f76c5
Mike Schwörer <pubgit@mikescher.com>
2020-12-03 17:46:22 +0100

    Day 03 - Problem 1

--- src/day03.rs
+++ src/day03.rs
@@ -1,29 +1,35 @@
 use crate::common::AdventOfCodeDay;
 
 pub struct Day03 {
-    //input: Vec<String>,
+    width: i32,
+    height: i32,
+    input: Vec<Vec<bool>>,
 }
 
 impl Day03 {
     pub fn new() -> Self {
-        //let input_bytes = include_bytes!("../res/03_input.txt");
-        //let input_str = String::from_utf8_lossy(input_bytes);
-        //
-        //let lines = input_str
-        //                .lines()
-        //                .map(|p| String::from(p))
-        //                .collect::<Vec<String>>();
+        let input_bytes = include_bytes!("../res/03_input.txt");
+        let input_str = String::from_utf8_lossy(input_bytes);
+        
+        let lines = input_str.lines().map(String::from).collect::<Vec<String>>();
 
         Self {
-            //input: lines
+            width: lines[0].len() as i32,
+            height: lines.len() as i32,
+
+            input: lines.iter().map(|l| l.chars().map(|c| c=='#').collect()).collect(),
         }
+    }
+
+    pub fn get(&self, x: i32, y: i32) -> bool {
+        return self.input[y as usize][(x % self.width) as usize]
     }
 }
 
 impl AdventOfCodeDay for Day03 {
 
     fn task_1(&self) -> String {
-        return "TODO".to_owned() //TODO
+        (0..self.height).filter(|y| self.get(3*y, *y)).count().to_string()
     }
 
     fn task_2(&self) -> String  {
