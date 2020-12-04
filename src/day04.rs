12926cb2495d762e266bb55d8e9c712fcc1534b0
Mike Schwörer <pubgit@mikescher.com>
2020-12-04 20:47:58 +0100

    Day 04 - Problem 2

--- src/day04.rs
+++ src/day04.rs
@@ -51,7 +51,7 @@ impl Day04 {
 }
 
 impl Passport {
-    pub fn is_valid(&self) -> bool {
+    pub fn is_all_present(&self) -> bool {
         if self.get("byr") == None { return false }
         if self.get("iyr") == None { return false }
         if self.get("eyr") == None { return false }
@@ -62,6 +62,56 @@ impl Passport {
         //if self.get("cid") == None { return false }
 
         return true
+    }
+
+    pub fn is_all_valid(&self) -> bool {
+        if self.get_and_validate("byr", r"^[12][90][0-9][0-9]$") == None { return false }
+        if self.get_and_validate("iyr", r"^20[12][0-9]$") == None { return false }
+        if self.get_and_validate("eyr", r"^20[23][0-9]$") == None { return false }
+        if self.get_and_validate("hgt", r"^[0-9]+(in|cm)$") == None { return false }
+        if self.get_and_validate("hcl", r"^#[0-9a-f]{6}$") == None { return false }
+        if self.get_and_validate("ecl", r"^(amb|blu|brn|gry|grn|hzl|oth)$") == None { return false }
+        if self.get_and_validate("pid", r"^[0-9]{9}$") == None { return false }
+        
+        let byr = self.get("byr").unwrap().parse::<i32>().unwrap();
+        let iyr = self.get("iyr").unwrap().parse::<i32>().unwrap();
+        let eyr = self.get("eyr").unwrap().parse::<i32>().unwrap();
+        let hgt = self.get("hgt").unwrap();
+
+        if byr < 1920 { return false }
+        if byr > 2002 { return false }
+
+        if iyr < 2010 { return false }
+        if iyr > 2020 { return false }
+
+        if eyr < 2020 { return false }
+        if eyr > 2030 { return false }
+
+        if hgt.ends_with("cm") {
+
+            let v = hgt.replace("cm", "").parse::<i32>().unwrap();
+            if v < 150 { return false }
+            if v > 193 { return false }
+
+        } else { //in
+
+            let v = hgt.replace("in", "").parse::<i32>().unwrap();
+            if v < 59 { return false }
+            if v > 76 { return false }
+
+        }
+
+        return true
+    }
+
+    pub fn get_and_validate(&self, key: &str, rex: &str) -> Option<String> {
+        if let Some(val) = self.get(key) {
+            let rex = Regex::new(rex).unwrap();
+            if rex.is_match(&val) {
+                return Some(val)
+            }
+        }
+        return None
     }
 
     pub fn get(&self, key: &str) -> Option<String> {
@@ -80,10 +130,10 @@ impl AdventOfCodeDay for Day04 {
         //    println!("{:?}", r);
         //    println!();
         //}
-        return self.input.iter().filter(|p| p.is_valid()).count().to_string() //TODO
+        return self.input.iter().filter(|p| p.is_all_present()).count().to_string()
     }
 
     fn task_2(&self) -> String  {
-        return "TODO".to_owned() //TODO
+        return self.input.iter().filter(|p| p.is_all_valid()).count().to_string()
     }
 }
