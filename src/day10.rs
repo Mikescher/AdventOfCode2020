8db99c8b9570e186f6d920498b54c6cc8d5c85a6
Mike Schwörer <pubgit@mikescher.com>
2020-12-10 18:29:44 +0100

    Day 10 - Problem 1

--- src/day10.rs
+++ src/day10.rs
@@ -1,21 +1,23 @@
 use crate::common::AdventOfCodeDay;
 
+use itertools::Itertools;
+
 pub struct Day10 {
-    //input: Vec<String>,
+    input: Vec<u32>,
 }
 
 impl Day10 {
     pub fn new() -> Self {
-        //let input_bytes = include_bytes!("../res/10_input.txt");
-        //let input_str = String::from_utf8_lossy(input_bytes);
-        //
-        //let lines = input_str
-        //                .lines()
-        //                .map(|p| String::from(p))
-        //                .collect::<Vec<String>>();
+        let input_bytes = include_bytes!("../res/10_input.txt");
+        let input_str = String::from_utf8_lossy(input_bytes);
+        
+        let data = input_str
+                        .lines()
+                        .map(|p| p.parse::<u32>().unwrap())
+                        .collect::<Vec<u32>>();
 
         Self {
-            //input: lines
+            input: data
         }
     }
 }
@@ -23,7 +25,27 @@ impl Day10 {
 impl AdventOfCodeDay for Day10 {
 
     fn task_1(&self) -> String {
-        return "TODO".to_owned() //TODO
+
+        let (min, max) = self.input.iter().minmax().into_option().unwrap();
+
+        verboseln!("min := {}", min);
+        verboseln!("max := {}", max);
+
+        let diff = self.input
+                       .iter()
+                       .chain([0, *max + 3].iter())
+                       .sorted()
+                       .collect::<Vec<&u32>>()
+                       .windows(2)
+                       .map(|p| p[1] - p[0])
+                       .collect::<Vec<u32>>();
+
+        let c1 = diff.iter().filter(|v| **v == 1).count();
+        let c3 = diff.iter().filter(|v| **v == 3).count();
+
+        verboseln!("{} * {} = {}", c1, c3, c1*c3);
+
+        return (c1 * c3).to_string() //TODO
     }
 
     fn task_2(&self) -> String  {
