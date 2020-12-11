93b507b3bce249ec7f6ccaeeaf8b4694f7e3b20a
Mike Schwörer <pubgit@mikescher.com>
2020-12-11 11:13:39 +0100

    Day 11 - Problem 2

--- src/day11.rs
+++ src/day11.rs
@@ -54,6 +54,47 @@ impl Board {
         return c;
     }
 
+    fn adjac2(&self, x: i32, y: i32) -> u32 {
+        let mut c = 0;
+
+        for dx in -1..=1 {
+            for dy in -1..=1 {
+                if dx == 0 && dy == 0 {
+                    continue;
+                }
+
+                if x+dx < 0 || y+dy < 0 || x+dx >= self.width as i32 || y+dy >= self.height as i32 {
+                    continue;
+                }
+                if self.adjac_ray(x, y, dx, dy) {
+                    c+=1;
+                }
+            }
+        }
+
+        return c;
+    }
+
+    fn adjac_ray(&self, x : i32, y: i32, dx: i32, dy: i32) -> bool {
+        for i in 1.. {
+            let rx = x + dx * i;
+            let ry = y + dy * i;
+
+            if rx < 0 || ry < 0 || rx >= self.width as i32 || ry >= self.height as i32 {
+                return false;
+            }
+            
+            if self.cells[ry as usize][rx as usize] == BoardCell::Full {
+                return true;
+            }
+            if self.cells[ry as usize][rx as usize] == BoardCell::Empty {
+                return false;
+            }
+        }
+
+        panic!();
+    }
+
     fn step(&self) -> Board {
 
         let mut nc = self.cells.clone();
@@ -66,6 +107,32 @@ impl Board {
                     }
                 } else if self.cells[y][x] == BoardCell::Empty {
                     if self.adjac(x as i32, y as i32) == 0 {
+                        nc[y][x] = BoardCell::Full; // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
+                    }
+                }
+            }
+        }
+
+        Board {
+            width: self.width,
+            height: self.height,
+
+            cells: nc,
+        }
+    }
+
+    fn step2(&self) -> Board {
+
+        let mut nc = self.cells.clone();
+
+        for y in 0..self.height {
+            for x in 0..self.width {
+                if self.cells[y][x] == BoardCell::Full {
+                    if self.adjac2(x as i32, y as i32) >= 5 {
+                        nc[y][x] = BoardCell::Empty; // it now takes five or more visible occupied seats for an occupied seat to become empty
+                    }
+                } else if self.cells[y][x] == BoardCell::Empty {
+                    if self.adjac2(x as i32, y as i32) == 0 {
                         nc[y][x] = BoardCell::Full; // If a seat is empty (L) and there are no occupied seats adjacent to it, the seat becomes occupied.
                     }
                 }
@@ -161,6 +228,22 @@ impl AdventOfCodeDay for Day11 {
     }
 
     fn task_2(&self) -> String  {
-        return "TODO".to_owned() //TODO
+        verboseln!("{}", self.input);
+        verboseln!("> Initial");
+
+        let mut board = self.input.clone();
+        loop {
+            let newboard = board.step2();
+
+            verboseln!("{}", newboard);
+            verboseln!("> {}", newboard.total_occupied());
+            verboseln!();
+
+            if newboard == board {
+                return newboard.total_occupied().to_string();
+            }
+
+            board = newboard;
+        }
     }
 }
