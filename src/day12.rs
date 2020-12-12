b20319c9212c9a084807937cbc7c7842041a426b
Mike Schwörer <pubgit@mikescher.com>
2020-12-12 10:42:42 +0100

    Day 12 - Problem 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,29 +1,82 @@
 use crate::common::AdventOfCodeDay;
 
+#[derive(Debug)]
+enum CType {
+    North,
+    East,
+    South,
+    West,
+    TurnRight,
+    TurnLeft,
+    Forward,
+}
+
+#[derive(Debug)]
 pub struct Day12 {
-    //input: Vec<String>,
+    input: Vec<(CType, i32)>,
 }
 
 impl Day12 {
     pub fn new() -> Self {
-        //let input_bytes = include_bytes!("../res/12_input.txt");
-        //let input_str = String::from_utf8_lossy(input_bytes);
-        //
-        //let lines = input_str
-        //                .lines()
-        //                .map(|p| String::from(p))
-        //                .collect::<Vec<String>>();
+        let input_bytes = include_bytes!("../res/12_input.txt");
+        let input_str = String::from_utf8_lossy(input_bytes);
+        
+        let data = input_str
+                        .lines()
+                        .map(Day12::parse_cmd)
+                        .collect::<Vec<(CType, i32)>>();
 
         Self {
-            //input: lines
+            input: data
         }
+    }
+
+    fn parse_cmd(line: &str) -> (CType, i32) {
+        (match &line[0..1] {
+            "N" => CType::North,
+            "E" => CType::East,
+            "S" => CType::South,
+            "W" => CType::West,
+            "L" => CType::TurnLeft,
+            "R" => CType::TurnRight,
+            "F" => CType::Forward,
+            _   => panic!(),
+        }, line[1..].parse::<i32>().unwrap())
     }
 }
 
 impl AdventOfCodeDay for Day12 {
 
     fn task_1(&self) -> String {
-        return "TODO".to_owned() //TODO
+        if is_verbose!() {
+            for e in &self.input { verboseln!("{:?}", e); }
+            verboseln!();
+            verboseln!();
+        }
+
+        let deltas: [(i32,i32); 4] = [(1,0),(0,1),(-1,0),(0,-1)];
+
+        let mut x: i32 = 0;
+        let mut y: i32 = 0;
+        let mut d: i32 = 0;
+
+        for cmd in &self.input {
+            
+            match cmd.0 {
+                CType::North     => { y -= cmd.1; },
+                CType::East      => { x += cmd.1; },
+                CType::South     => { y += cmd.1; },
+                CType::West      => { x -= cmd.1; },
+                CType::TurnLeft  => { d = (d + 16 - (cmd.1 / 90)) % 4 },
+                CType::TurnRight => { d = (d + 16 + (cmd.1 / 90)) % 4 },
+                CType::Forward   => { x += deltas[d as usize].0 * cmd.1; y += deltas[d as usize].1 * cmd.1; },
+            }
+
+            verboseln!("[{},{}|{}] {:?}", x, y, d, cmd);
+
+        }
+
+        return (x.abs() + y.abs()).to_string();
     }
 
     fn task_2(&self) -> String  {
